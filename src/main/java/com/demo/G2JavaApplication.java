b743b5e6c855f07c9346d237d4e2e81e81486ba3
Yomo <davivimix96@gmail.com>
2026-04-22 12:31:38 +0200

    Hola

--- src/main/java/com/demo/G2JavaApplication.java
+++ src/main/java/com/demo/G2JavaApplication.java
@@ -3,6 +3,7 @@ package com.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//hola
 @SpringBootApplication
 public class G2JavaApplication {
 
