6005d86cf951ce7bcabc4c453f5afc98b1a169b8
alansastre <alan@certidevs.com>
2026-04-22 11:54:54 +0200

    Proyecto setup

---
+++ src/main/java/com/demo/G2JavaApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G2JavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(G2JavaApplication.class, args);
+    }
+
+}
